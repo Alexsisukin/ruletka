7acf59c26acee2bb15bedcc6a78d6b57cf40b2d4
Александр Сисюкин <sisukinucoz@gmail.com>
2018-05-15 23:33:12 +0300

    info

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
-
+phpinfo();
+exit();
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
@@ -19,7 +20,7 @@
 |
 */
 
-require __DIR__.'/../bootstrap/autoload.php';
+require __DIR__ . '/../bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -33,7 +34,7 @@ require __DIR__.'/../bootstrap/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
